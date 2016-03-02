56785b06e9b49cdcc3a731e2416c95e1769067ac
Danny Willems <contact@danny-willems.be>
2016-03-02 15:12:13 +0100

    update the class type

--- vibration.mli
+++ vibration.mli
@@ -1,7 +1,7 @@
 class type navigator =
   object
     method vibrate : int -> bool Js.t Js.meth
-    method vibrateWithPattern : int Js.js_array Js.t -> int -> unit Js.meth
+    method vibrate_pattern : int Js.js_array Js.t -> unit Js.meth
     method cancelVibration : unit -> unit Js.meth
   end
 
