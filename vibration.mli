89c8e1815a9d17de10d049ab1c23f7d9deb05196
Danny Willems <contact@danny-willems.be>
2016-03-29 19:32:40 +0200

    update to gen_js_api

--- vibration.mli
+++ vibration.mli
@@ -1,8 +1,16 @@
-class type vibration =
+(* -------------------------------------------------------------------------- *)
+class vibration : Ojs.t ->
   object
-    method vibrate : int -> bool Js.t Js.meth
-    method vibrate_pattern : int Js.js_array Js.t -> unit Js.meth
-    method cancelVibration : unit Js.meth
+    inherit Ojs.obj
+    method vibrate            : int       -> bool
+    method vibrate_pattern    : int array -> bool
+    [@@js.call "vibrate"]
+    method cancel   : unit
+    [@@js.call "cancel"]
   end
+(* -------------------------------------------------------------------------- *)
 
-val vibration : unit -> vibration Js.t
+(* -------------------------------------------------------------------------- *)
+val t : unit -> vibration
+[@@js.get "navigator"]
+(* -------------------------------------------------------------------------- *)
